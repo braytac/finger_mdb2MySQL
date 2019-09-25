349ad2792a5747f5e08404959bd8973973df6f7d
Henry <zat_xorg@hotmail.com>
2019-09-25 15:00:49 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 SSH_USER = ''
+SSH_PWD = ''
 URL = ''
 DB = ''
 MYSQL_USER = ''
 MYSQL_PASS = ''
+MDB_FILE = ''
+
+DIFF_IDS = 916594
+PORCION = 40000
+# INSTALAR AccessDatabaseEngine.exe:
+# https://www.microsoft.com/es-es/download/confirmation.aspx?id=13255
+# python -m py_compile File1.py File2.py File3.py
